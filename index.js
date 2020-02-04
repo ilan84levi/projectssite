5e93cb6a290756d6796362d52b6cce1dadf214b6
Ilan Levi <42164441+ilan84levi@users.noreply.github.com>
2020-02-05 01:09:11 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+
+// let quotes;
+// $(document).ready(() => {
+//  quotes = $("#randomQuote");
+// });
+
+
+// setInterval(()=>{
+// $.ajax({
+//     method: "GET",
+//     url: "http://api.icndb.com/jokes/random/",
+//     success: function(response) {
+        
+//         console.log(  response.value.joke)
+//        quotes.text(response.value.joke)
+
+//     },
+//     error: function(err) {
+//         alert("Error: " + err.status)
+//     }
+// });
+// },3000)
+
+
+
+
+
